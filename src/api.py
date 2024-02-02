aedb1ef94a9a902af58c57bcecb76bf0b8e8974a
longwind48 <longwind48@gmail.com>
2024-02-02 21:59:45 +0800

    refactor: fix wrong def for wandb login

--- src/api.py
+++ src/api.py
@@ -34,9 +34,8 @@ async def lifespan(app: FastAPI):
     if not os.path.exists(model_path):
         # Download the model from wandb if it's not already downloaded
         try:
-            run = wandb.init(
-                project=PROJECT_NAME, job_type="inference", token=os.getenv("WANDB_API_KEY")
-            )
+            wandb.login(key=os.getenv("WANDB_API_KEY"))
+            run = wandb.init(project=PROJECT_NAME, job_type="inference")
             logger.info(f"Loading model from {REGISTERED_MODEL_NAME}...")
             artifact = run.use_artifact(
                 f"model-registry/{REGISTERED_MODEL_NAME}:{ALIASES[0]}", type="model"
